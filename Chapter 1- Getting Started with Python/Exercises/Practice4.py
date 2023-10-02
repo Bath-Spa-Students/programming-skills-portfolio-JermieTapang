cece840961494e5aef18317d979b7396363e2dd7
JermieTapang <jermietapang96@gmail.com>
2023-10-02 12:02:19 +0400

    Excerises2

---
+++ Chapter 1- Getting Started with Python/Exercises/Practice4.py
@@ -0,0 +1,8 @@
+#Compute the area of a triangle
+a= 5
+b= 8
+c=4
+
+s = (a+b+c)/2
+area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
+print ('The area of Triangle is %0.2f' %area)
---
+++ Chapter 1- Getting Started with Python/Exercises/Practice5.py
@@ -0,0 +1,11 @@
+#Write four strings in different varuables and print the output as one string (string Concatenation)
+str1="Jermie"
+str2=" Carl"
+str3=" Briones"
+str4=" Tapang"
+print ("string1:",str1)
+print ("string2:",str2)
+print ("string3:",str3)
+print ("string4:",str4)
+str=str1+str2+str3+str4
+print ("Concatenated two different strings:",str)
