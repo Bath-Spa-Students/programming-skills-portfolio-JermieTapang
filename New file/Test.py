c3666f0a528cdfa76a683466fe827c885687d260
JermieTapang <jermietapang96@gmail.com>
2023-11-23 12:19:51 +0400

    Test.py

---
+++ New file/Test.py
@@ -0,0 +1,54 @@
+## 1. Write a program to take 5 numbers as an input from the use and print the sum and average.
+
+number_input1 = int(input("Please write a number :"))
+number_input2 = int(input("PLease write your second number :"))
+number_input3 = int(input("Please write your third number :"))
+number_input4 = int(input("Please write your fourth number :"))
+number_input5 = int(input("PLease write your last number :"))
+
+Final_answer = (number_input1+number_input2+number_input3+number_input4+number_input5)
+
+print (Final_answer)
+
+## 2. Write a program in which you can display list of your favorite things and print the list.
+
+favorite_things = ['Badminton', 'Swimming', 'Table Tenis', 'Mango', 'Lychee', 'Watermelon']
+print (favorite_things)
+
+## 3. Create a nested dictionary to store the idem category, item name, item code, price, and stock.
+# And Display the list of the items using fuctions.
+
+Snacks = { 
+    "Snack1" : { 
+    "brand" : "Doritos",
+    "price" : "5.50 AED"
+    },
+    "Snack2" : {
+    "brand" : "Lays",
+    "price" : "4.75 AED"
+    },
+    "Snack3" : { 
+    "brand" : "Cheetos",
+    "price" : "3.25"
+    }
+}
+
+print (Snacks)
+
+
+Drinks = { 
+    "Drinks1" : { 
+    "brand" : "Arwa",
+    "price" : "1.50 AED"
+    },
+    "Drinks2" : {
+    "brand" : "MayDubai",
+    "price" : "1.75 AED"
+    },
+    "Drinks3" : { 
+    "brand" : "Oasis",
+    "price" : "1.25"
+    }
+}
+
+print (Drinks)
